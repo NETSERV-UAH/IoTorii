9b97d27e8ad0139ffe238d5a38991dac9fe47f9b
Hedayat Hosseini <h.hosseini@aut.ac.ir>
2018-12-28 23:05:20 +0330

    A new version to handle all neighbors

---
+++ Contiki-ng_4_2/iotorii-n-hlmac-2/common-node/start-iotorii-common-node.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 Elisa Rojas(1), Hedayat Hosseini(2), and David Carrascal(1);
+ *                    (1) GIST, University of Alcala, Spain.
+ *                    (2) CEIT, Amirkabir University of Technology (Tehran
+ *                        Polytechnic), Iran.
+ * Adapted to use IoTorii, a link layer protocol for Low pawer and Lossy Network
+ * (LLN), over the IEEE 802.15.4 none beacon mode.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *
+ */
+
+/**
+ * \file
+ *         This process starts the IoTorii mac protocol
+ */
+
+#include "contiki.h"
+#include "net/netstack.h"
+#include <stdio.h>
+
+
+/*---------------------------------------------------------------------------*/
+PROCESS(start_iotorii_common_node, "Process to Start common node");
+AUTOSTART_PROCESSES(&start_iotorii_common_node);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(start_iotorii_common_node, ev, data)
+{
+
+  PROCESS_BEGIN();
+  
+  NETSTACK_RADIO.on();
+  //NETSTACK_MAC.init(); //MAC is initiated twice
+  //netstack_init(); //MAC is initiated twice
+
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/

cbba653b10c9d1e1927c2dc8c1f5cad78ba52a14
Pablo Collado <pcolladosoto@gmail.com>
2019-07-10 21:20:11 +0200

    Monitoring working

--- Contiki-ng_4_2/iotorii-n-hlmac/code/common-node/iotorii-common-node.c
+++ Contiki-ng_4_2/iotorii-n-hlmac/code/common-node/iotorii-common-node.c
@@ -43,10 +43,11 @@
 #include "contiki.h"
 #include "net/netstack.h"
 #include <stdio.h>
-
+#include "sys/energest.h"
 
 /*---------------------------------------------------------------------------*/
 PROCESS(start_iotorii_common_node, "Process to Start common node");
+PROCESS(energy_tracking, "Energy tracking through the Energest module");
 AUTOSTART_PROCESSES(&start_iotorii_common_node, &energy_tracking);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(start_iotorii_common_node, ev, data)
@@ -61,4 +62,41 @@ PROCESS_THREAD(start_iotorii_common_node, ev, data)
 
   PROCESS_END();
 }
+
 /*---------------------------------------------------------------------------*/
+
+PROCESS_THREAD(energy_tracking, ev, data) {
+    static struct etimer energy_timer;
+
+    PROCESS_BEGIN();
+
+    etimer_set(&energy_timer, REFRESH_INTERVAL * CLOCK_SECOND);
+
+    while (1) {
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&energy_timer));
+        etimer_reset(&energy_timer);
+
+        energest_flush();
+
+        printf("Uptimes\n#######\n\tCPU times:\n");
+        printf("\t\tACTIVE: %4lus\tLOW POWER: %4lus\tDEEP SLEEP: %4lus\t TOTAL: %4lus\n\n",
+                (unsigned long) (energest_type_time(ENERGEST_TYPE_CPU) / ENERGEST_SECOND),
+                (unsigned long) (energest_type_time(ENERGEST_TYPE_LPM) / ENERGEST_SECOND),
+                (unsigned long) (energest_type_time(ENERGEST_TYPE_DEEP_LPM) / ENERGEST_SECOND),
+                (unsigned long) (ENERGEST_GET_TOTAL_TIME() / ENERGEST_SECOND));
+        printf("\tRADIO times:\n\t\tTX: %4lus\tRX: %4lus\tOFF: %4lus\n\n",
+                (unsigned long) (energest_type_time(ENERGEST_TYPE_TRANSMIT) / ENERGEST_SECOND), 
+                (unsigned long) (energest_type_time(ENERGEST_TYPE_LISTEN) / ENERGEST_SECOND),
+                (unsigned long) ((ENERGEST_GET_TOTAL_TIME() -
+                energest_type_time(ENERGEST_TYPE_TRANSMIT) - 
+                energest_type_time(ENERGEST_TYPE_LISTEN)) / ENERGEST_SECOND));
+        printf("Energy consumptions\n###################\n\tCPU: %lg mJ\n",
+                (double) (MCU_VOLTAGE * (energest_type_time(ENERGEST_TYPE_CPU) * ACTIVE_CURRENT +
+                          energest_type_time(ENERGEST_TYPE_LPM) * SLEEP_CURRENT +
+                          energest_type_time(ENERGEST_TYPE_DEEP_LPM) * DEEP_SLEEP_CURRENT)));
+        printf("\tRADIO: %lg mJ\n",
+                (double) (energest_type_time(ENERGEST_TYPE_TRANSMIT) * TX_POWER +
+                          energest_type_time(ENERGEST_TYPE_LISTEN) * RX_POWER));
+    }
+    PROCESS_END();
+}
--- Contiki-ng_4_2/iotorii-n-hlmac/code/common-node/project-conf.h
+++ Contiki-ng_4_2/iotorii-n-hlmac/code/common-node/project-conf.h
@@ -111,5 +111,15 @@
 /*---------------------------------------------------------------------------*/
 #ifdef TRACK_ENERGY
 #define ENERGEST_CONF_ON 1
+
+#define REFRESH_INTERVAL 10
+
+#define MCU_VOLTAGE 5 // V
+#define ACTIVE_CURRENT 20 // mA
+#define SLEEP_CURRENT 10 // mA
+#define DEEP_SLEEP_CURRENT 5 // mA
+
+#define TX_POWER 10 // mW
+#define RX_POWER 5 // mW
 #endif
 #endif /* PROJECT_CONF_H_ */
